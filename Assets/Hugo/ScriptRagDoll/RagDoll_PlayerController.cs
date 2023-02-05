c35af1daa390a30e4c1fd3cb2ba74dba088b8ab0
Victor Aine Todeschini <victorat73@gmail.com>
2023-02-05 04:30:03 +0100

    Modif déplacements ( la rotation est à l'interieur)

--- Assets/Hugo/ScriptRagDoll/RagDoll_PlayerController.cs
+++ Assets/Hugo/ScriptRagDoll/RagDoll_PlayerController.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 public class RagDoll_PlayerController : MonoBehaviour
 {
     public float speed;
-    public float strafeSpeed;
     public float jumpForce;
 
     public Rigidbody hips;
 
     public bool isGrounded;
 
-    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -18,51 +16,15 @@ public class RagDoll_PlayerController : MonoBehaviour
         hips = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+
+    private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
 
-        if (verticalInput != 0)
-        {
-            hips.AddForce(hips.transform.forward * speed);
-        }
-        
-        if (horizontalInput != 0)
-        {
-            hips.AddForce(-hips.transform.forward * strafeSpeed);
-        }
-        /*
-        if (verticalInput < 0)
-        {
-            hips.AddForce(-hips.transform.forward * speed);
-        }
-
-        if (horizontalInput > 0)
-        {
-            hips.AddForce(hips.transform.right * strafeSpeed);
-        }
-        */
-        if (Input.GetKey(KeyCode.Space))
-        {
-            if (isGrounded)
-            { 
-                hips.AddForce(new Vector3(0, jumpForce, 0));
-                isGrounded = false;
-            }
-        }
-    }
-
-    private void Update()
-    {
-        if (hips.velocity.x > 0.5f || hips.velocity.x < -0.5f || hips.velocity.z > 0.5f || hips.velocity.z < -0.5f)
-        {
-            animator.SetBool("isWalking", true);
-        }
-        else
-        {
-            animator.SetBool("isWalking", false);
-        }
+            Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput);
+            hips.velocity = direction * speed;
+            this.transform.forward = direction;
     }
 }
