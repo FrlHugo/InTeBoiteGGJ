3029018cdc64babf60b57c46e82e73c0c00ec309
Louis BICH <louis.bich@gmail.com>
2023-02-05 12:05:50 +0100

    pull

---
+++ Assets/ZoneTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneTrigger : MonoBehaviour
+{
+    public NarrateurManager narrateurManager;
+
+    public int epreuveIndex;
+    public int missionIndex;
+
+
+    private void Start()
+    {
+        narrateurManager = FindObjectOfType<NarrateurManager>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item") || other.gameObject.CompareTag("Tunique"))
+        {
+            narrateurManager.epreuves[epreuveIndex].missions[missionIndex] = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item") || other.gameObject.CompareTag("Tunique"))
+        {
+            narrateurManager.epreuves[epreuveIndex].missions[missionIndex] = false;
+        }
+    }
+}
